28cdd66605583624809e7c966013dc9b9ea2cff9
Shigeo-Kawai <71721381+Shigeo-Kawai@users.noreply.github.com>
2020-09-23 01:41:29 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,44 @@
+//アルバムデータの作成
+var album = [
+  {src: 'img/_MG_9813.JPG', msg: ''},
+  {src: 'img/_MG_9780.JPG', msg: ''},
+  {src: 'img/IMG_7734.JPG', msg: ''},
+  {src: 'img/IMG_7837.JPG', msg: ''},
+  {src: 'img/IMG_7838.JPG', msg: ''},
+  {src: 'img/IMG_8232.JPG', msg: ''},
+  {src: 'img/_MG_9139.JPG', msg: ''},
+  {src: 'img/92880036.jpg', msg: ''},
+  {src: 'img/92880033.jpg', msg: ''},
+  {src: 'img/_MG_1544.JPG', msg: ''},
+  {src: 'img/_MG_1558.JPG', msg: ''},
+  {src: 'img/_MG_9563.JPG', msg: ''},
+];
+
+//最初のデータを表示しておく
+var mainImage = document.createElement('img');
+mainImage.setAttribute('src', album[0].src);
+mainImage.setAttribute('alt', album[0].msg);
+
+var mainMsg = document.createElement('p');
+mainMsg.innerText = mainImage.alt;
+
+var mainFlame = document.querySelector('#gallery .main');
+mainFlame.insertBefore(mainImage, null);
+mainFlame.insertBefore(mainMsg, null);
+
+//サムネイル写真画像の表示
+var thumbFlame = document.querySelector('#gallery .thumb');
+for (var i = 0; i < album.length; i++) {
+  var thumbImage = document.createElement('img');
+  thumbImage.setAttribute('src', album[i].src);
+  thumbImage.setAttribute('alt', album[i].msg);
+  thumbFlame.insertBefore(thumbImage, null);    
+}
+
+//クリックした画像をメインにする
+thumbFlame.addEventListener('click', function(event){
+  if (event.target.src) {
+    mainImage.src = event.target.src;
+    mainMsg.innerText = event.target.alt;
+  }
+});
